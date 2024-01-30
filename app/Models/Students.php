f8659b2df1ac8462b6a21a503d622b145065fdb2
Abdul Rahman <tugas.abdulrahman@gmail.com>
2024-01-30 11:28:25 +0700

    add navigation students

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Students extends Model
 {
     use HasFactory;
+
+    protected $table = 'students';
+    protected $primaryKey = 'idstudents';
+
+    // mematikan autoincrement
+    public $incrementing = false;
+    public $timestamps = true;
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('students')" :active="request()->routeIs('students')">
+                        Students
+                    </x-nav-link>
+                             
                 </div>
             </div>
 
