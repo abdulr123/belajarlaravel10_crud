17f193b54afd32e2a94624d1c7d143a50f8e04a9
Abdul Rahman <tugas.abdulrahman@gmail.com>
2024-01-30 15:06:41 +0700

    menampilkan data students

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,12 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-
                     <x-nav-link :href="route('students')" :active="request()->routeIs('students')">
-                        Students
+                        {{ __('Students Data') }}
                     </x-nav-link>
-                             
                 </div>
+
+
             </div>
 
             <!-- Settings Dropdown -->
--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -1,15 +1,51 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Students') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <h4>Halaman Students</h4>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-3">
+                <div class="p-2 text-gray-900">
+                    <h4>Students Data</h4>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-sm table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Id Students</th>
+                                    <th>Full Name</th>
+                                    <th>Genre</th>
+                                    <th>Adress</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($students as $row)
+                                    <tr>
+                                        <th>{{ $loop->iteration }}</th>
+                                        <td>{{ $row->idstudents }}</td>
+                                        <td>{{ $row->fullname }}</td>
+                                        <td>{{ $row->gender == 'M' ? 'Male' : 'Female' }}</td>
+                                        <td>{{ $row->address }}</td>
+                                        <td>{{ $row->emailaddress }}</td>
+                                        <td>{{ $row->phone }}</td>
+                                        <td>                                           
+                                            <i class="fa fa-pencil-square-o"  aria-hidden="true" > | 
+                                            <i class="fa fa-trash" aria-hidden="true"></i>                                              
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                            
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
