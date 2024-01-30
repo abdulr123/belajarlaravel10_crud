23f1a1fcb3e0eeb6acdca86a6f83e82486d5ab2a
Abdul Rahman <tugas.abdulrahman@gmail.com>
2024-01-30 20:45:37 +0700

    add deskripsi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
